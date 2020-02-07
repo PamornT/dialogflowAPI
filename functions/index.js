3d28d66cd22944cd8dbaa1114d71e9d838c80113
PamornT <chocobobu@gmail.com>
2020-02-07 21:15:58 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,22 @@
-const region = 'asia-northeast1';
-const functions = require('firebase-functions');
-const request = require('request-promise');
-const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
+//----- C H A N G E -------//
+const CHANNEL_ACCESS_TOKEN = ""
+const DIALOGFLOW_PROJECTID = ""
+const DIALOGFLOW_SERVICE_ACCOUNT = ""
+//------------------------//
+
+const region = 'asia-northeast1'
+const functions = require('firebase-functions')
+const request = require('request-promise')
+const dialogflow = require('dialogflow')
+const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message"
 const LINE_HEADER = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer CHANNEL_ACCESS_TOKEN"
-};
-
-//  1. Import Dialogflow library
-
-//  2. define dialogflow projectId
-
-//  3. Create session client
+    "Authorization": `Bearer ${CHANNEL_ACCESS_TOKEN}`
+}
+const sessionClient = new dialogflow.SessionsClient({
+    DIALOGFLOW_PROJECTID,
+    keyFilename: DIALOGFLOW_SERVICE_ACCOUNT,
+})
 
 exports.webhook = functions.region(region).https.onRequest(async (req, res) => {
     if (req.body.events[0].type !== 'message') {
@@ -24,17 +29,28 @@ exports.webhook = functions.region(region).https.onRequest(async (req, res) => {
     const userId = event.source.userId
     const message = event.message.text
     
-    //  7. call detectIntent function
+    const intentResponse = await detectIntent(userId, message, 'th')
     
-    //  8. convert structure to json
-    
+    const structjson = require('./structjson');
+    const intentResponseMessage = intentResponse.queryResult.fulfillmentMessages
+    const replyMessage = intentResponseMessage.map( (messageObj) => {
+        let struct
+        if (messageObj.message === "text") {
+            return {type: "text", text: messageObj.text.text[0] }
+        } else if(messageObj.message === "payload") {
+            struct = messageObj.payload
+            return structjson.structProtoToJson(struct)
+        }
+        return null
+    })
+
     request({
         method: "POST",
         uri: `${LINE_MESSAGING_API}/reply`,
         headers: LINE_HEADER,
         body: JSON.stringify({
             replyToken: event.replyToken,
-            messages: [{"type":"text","text":message}]
+            messages: replyMessage
         })
     })
     
@@ -42,9 +58,16 @@ exports.webhook = functions.region(region).https.onRequest(async (req, res) => {
 })
 
 const detectIntent = async (userId, message, languageCode) => {
-    //  4. create session path เพื่อจดจำ context ของ user
-    
-    //  5. create request params เพื่อใช้ส่งไป Dialogflow เพื่อ detectIntent
-    
-    //  6. call dialogflow API detectIntent
+    const sessionPath = sessionClient.sessionPath(DIALOGFLOW_PROJECTID, userId)
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: languageCode,
+            },
+        },
+    }
+    const responses = await sessionClient.detectIntent(request)
+    return responses[0]
 }
